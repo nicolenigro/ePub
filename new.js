daade8b0e1043d99c6b4349517e18ab586c8be74
Anaïs Sarrazin <acsarraz@bowdoin.edu>
2020-04-01 17:27:03 -0700

    Update new.js

--- new.js
+++ new.js
@@ -22,7 +22,7 @@ var shoppingCart = (function() {
     sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
   }
   
-  // Load cart
+    // Load cart
   function loadCart() {
     cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
   }
@@ -95,7 +95,7 @@ var shoppingCart = (function() {
     for(var item in cart) {
       totalCount += cart[item].count;
     }
-    return Number(totalCount.toFixed(2));
+    return totalCount;
   }
 
   // Total cart
@@ -163,13 +163,13 @@ function displayCart() {
   for(var i in cartArray) {
     output += "<tr>"
       + "<td>" + cartArray[i].name + "</td>" 
-      + "<td>(" + "$" + cartArray[i].price + ")</td>"
+      + "<td>(" + cartArray[i].price + ")</td>"
       + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
       + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
       + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
       + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
       + " = " 
-      + "<td>" + "$" + cartArray[i].total + "</td>" 
+      + "<td>" + cartArray[i].total + "</td>" 
       +  "</tr>";
   }
   $('.show-cart').html(output);
@@ -178,6 +178,7 @@ function displayCart() {
 }
 
 // Delete item button
+
 $('.show-cart').on("click", ".delete-item", function(event) {
   var name = $(this).data('name')
   shoppingCart.removeItemFromCartAll(name);
