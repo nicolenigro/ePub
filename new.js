9553f0d5758b71a3af654143f8e0cf37b91fbefd
Anaïs Sarrazin <acsarraz@bowdoin.edu>
2020-04-03 13:40:40 -0700

    Update new.js

--- new.js
+++ new.js
@@ -176,7 +176,7 @@ function displayCart() {
   var output = "";
   //var item = cartArray[i].name
   for(var i in cartArray) {
-    var item = item[cartArray[i].name]
+    var item = app[cartArray[i].name]
     output += "<tr>"
       + "<td>" + item + "</td>" 
       + "<td>(" + "$" + cartArray[i].price + ")</td>"
