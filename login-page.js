4910253f428b39a94ae5276fede93380f6f46319
Anaïs Sarrazin <acsarraz@bowdoin.edu>
2020-05-01 19:29:39 -0700

    login

--- login-page.js
+++ login-page.js
@@ -7,9 +7,12 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "acsarraz" && password === "anais") {
+    if (username === "asarrazin" && password === "anais") {
         window.location.replace('menu.html')
-        //location.reload();
+    } else if(username === "emccabe" && password === "emme") {
+    	window.location.replace('menu.html')
+    } else if(username === "nnigro" && password === "nicole") {
+    	window.location.replace('menu.html')
     } else {
         //loginErrorMsg.style.opacity = 1;
         alert("Error password or username!")
